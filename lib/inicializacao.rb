604501ce537317fa9fef98d6f18458fcec3be0aa
Thiago Almeida <103385882+Codeinsid@users.noreply.github.com>
2022-09-22 15:47:39 -0300

    Update inicializacao.rb

--- lib/inicializacao.rb
+++ lib/inicializacao.rb
@@ -1,13 +1,39 @@
 class Inicializacao
 	def self.inicializando
 		system('cls') # cls para windows clear para linux
-		print "Inicializando."
-		4.times do |i|
-		sleep 1
-		print "."
-		end
-		puts "."
-		
+#		print "Inicializando."
+#		4.times do |i|
+#		sleep 1
+#		print "."
+#		end
+#		puts "."
+puts "	               .'         `\			    """
+puts "	              /   _         \			    """""
+puts "	 .-""-.       |  (O\.--.-.-/O)          .-""-.      """""""
+puts "	/ O O  \      .\|(_._.__._.__)         /  O O \	    "
+puts "	|O .-.  \    (   )   0 _ 0   \        /  .-. O|     "
+puts "	\ (   )  '.   `-|     (_)     |     .'  (   ) /     "
+puts "	'.`-'     '-./`|             |`\.-'     '-'.'       "
+puts "	   \         |  \   \     /   /  |         /        "
+puts "	    \        \   '.  '._.'  .'   /        /         "
+puts "	     \        '.   `'-----'`   .'        /          "
+puts "	      \   .'    '-._        .-'\   '.   /           "
+puts "   	   |/`          `'''''')    )    `\|        "
+puts "	       /                  (    (      ,\            "
+puts "	      ;                    \    '-..-'/ ;           "
+puts "	      |                     '.       /  |           "
+puts "	      |                       `'---'`   |           "
+puts "	      ;                                 ;           "
+puts "	       \                               /            "
+puts "	        `.                           .'             "
+puts "	          '-._                   _.-'               "
+puts "	    	    __/`"  '  - - -  ' "`` \__              "
+puts "	         /`            /^\           `\             "
+puts "	         \(          .'   '.         )/             "
+puts "	   	       '.(__(__.-'       '.__)__).'	    "
+
+puts "-----------------------------------------------------"
+		sleep 10
 		system('cls')
 	end
 end
